80d4ba104898db83a9c1217876e08f25ab48ef8e
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-09-11 13:59:21 +0300

    Add default file for start todo program

--- todo_list.rb
+++ todo_list.rb
@@ -11,19 +11,30 @@ class TodoList
 
   def instruction
     puts "----------TO LIST-------------".color(:yellow)
-    puts "All command arguments should be separated by comma".color(:red)
+    puts "---All todo files---".color(:orange)
+    show_todo_lists
+    puts "--------------------".color(:orange)
+    puts "All command arguments should be separated by comma".color(:orange)
     puts "Commands: ".color(:yellow)
     puts "new_todo, file_name".color(:blue) + " - creates todo list file".color(:yellow)
     puts "delete_todo, file_name".color(:blue) + " - removes todo list file".color(:yellow)
     puts "add, task, person".color(:blue) + " - adds task to todo list with assignee person".color(:yellow)
     puts "remove, task number".color(:blue) + " - removes task from todo list".color(:yellow)
     puts "status, task number, status".color(:blue) + " - changes tasks status".color(:yellow)
-    puts "assign, task number, person".color(:blue) + " - adds responsible person if he was not previously appointed or reassign for one task".color(:yellow)
+    puts "assign, task number, person".color(:blue) + " - adds assignee person if not assigned or reassign for one task".color(:yellow)
     puts "start, task number".color(:blue) + " - starts time tracking".color(:yellow)
     puts "stop, task number".color(:blue) + " - starts time tracking".color(:yellow)
     puts "save".color(:blue) + " - saves changes in file".color(:yellow)
     puts "up, task number".color(:blue) + " - moves task up".color(:yellow)
     puts "down, task number".color(:blue) + " - moves task down".color(:yellow)
+  end
+
+  def show_todo_lists
+    entries = Dir.entries("./todo_folder")
+    entries.delete(".")
+    entries.delete("..")
+    entries.shift
+    entries.each { |f| puts f.color(:green) }
   end
 
   def create(file_name)
